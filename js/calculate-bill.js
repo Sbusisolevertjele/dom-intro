521241e88edfb9562c21cd23fe5bfff0ef4b3fdc
Sbusisolevertjele <82034584+Sbusisolevertjele@users.noreply.github.com>
2021-08-24 09:34:51 +0200

    updated work

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -11,3 +11,27 @@
 //  * once done looping over all the entries - display the total onto the screen in the billTotal element
 
 //link the function to a click event on the calculate button
+
+var calculateBtn = document.querySelector('.calculateBtn');
+var billTotalElem = document.querySelector('.billTotal');
+var billString = document.querySelector('.billString');
+
+var billTotal = 0;
+
+function totalBill() {
+    var billElements = billString.value.split(",");
+    billTotal = 0;
+    billElements.forEach(function (element) {
+        var trimmedElement = element.trim();
+        if (trimmedElement === "call") {
+            billTotal += 2.75
+        }
+        else if (trimmedElement === "sms") {
+            billTotal += 0.75;
+        }
+    });
+
+    billTotalElem.innerHTML = billTotal.toFixed(2);
+}
+
+calculateBtn.addEventListener("click", totalBill);
